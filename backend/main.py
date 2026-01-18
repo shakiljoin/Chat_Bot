b945ea1b9d79cd9853361bbbf75d5a552e0fafb9
Shakil Ahamed <shakil.joinup@gmail.com>
2026-01-18 19:04:44 +0530

    new fix backend

--- backend/main.py
+++ backend/main.py
@@ -20,8 +20,10 @@ app = FastAPI()
 # ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "chat-bot-psi-gules.vercel.app"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://chat-bot-psi-gules.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
